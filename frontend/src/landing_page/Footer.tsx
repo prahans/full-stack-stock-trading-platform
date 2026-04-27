f6aefefeb881392068ce9d21140e6d1e9ca561fd
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-04-27 13:51:04 +0545

    building footer

--- frontend/src/landing_page/Footer.tsx
+++ frontend/src/landing_page/Footer.tsx
@@ -1,7 +1,469 @@
 function Footer() {
-    return ( 
-        <h1>Footer</h1>
-     );
+  return (
+    <footer style={{ backgroundColor: "rgb(250, 250, 250)" }}>
+      <div className="container border-top">
+        <div className="row mt-5">
+          <div className="col">
+            <img src="media\images\logo.svg" style={{ width: "50%" }} />
+            <p className="text-muted" style={{ fontSize: "14px" }}>
+              &copy; 2010 - 2026, Zerodha Broking Ltd. All rights reserved.
+            </p>
+          </div>
+          <div className="col">
+            <h5 className="mb-4" style={{ opacity: ".9" }}>
+              Account
+            </h5>
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Open demat account
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Minor demat account
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              NRI demat account
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              HUF demat account
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Commodity
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Dematerialisation
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Fund transfer
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              MTF
+            </a>
+            <br />
+          </div>
+          <div className="col">
+            <h5 className="mb-4" style={{ opacity: ".9" }}>
+              Support
+            </h5>
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Contact us
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Support portal
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              How to file a complaint?
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Status of your complaints
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Bulletin
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Circular
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Z-Connect blog
+            </a>
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Downloads
+            </a>
+            <br />
+          </div>
+          <div className="col">
+            <h5 className="mb-4" style={{ opacity: ".9" }}>
+              Company
+            </h5>
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              About
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Philosophy
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Press & media
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Careers
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Zerodha Cares (CSR)
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Zerodha.tech
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Open source
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Referral program
+            </a>{" "}
+            <br />
+          </div>
+          <div className="col">
+            <h5 className="mb-4" style={{ opacity: ".9" }}>
+              Quick links
+            </h5>
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Upcoming IPOs
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Brokerage charges
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Market holidays
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Economic calendar
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Calculators
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Markets
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Sectors
+            </a>{" "}
+            <br />
+            <a
+              href=""
+              style={{ textDecoration: "none", display: "inline-block" }}
+              className="text-muted mb-2"
+            >
+              Gift Nifty
+            </a>{" "}
+            <br />
+          </div>
+        </div>
+        <div className="mt-5 mb-5">
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI
+            Registration no.: INZ000031633 CDSL/NSDL: Depository services
+            through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019
+            Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross,
+            Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase,
+            Bengaluru - 560078, Karnataka, India. For any complaints pertaining
+            to securities broking please write to{" "}
+            <a href="" style={{ textDecoration: "none", opacity: ".8" }}>
+              complaints@zerodha.com
+            </a>
+            , for DP related to{" "}
+            <a href="" style={{ textDecoration: "none", opacity: ".8" }}>
+              dp@zerodha.com
+            </a>
+            . Please ensure you carefully read the Risk Disclosure Document as
+            prescribed by SEBI | ICF
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            Procedure to file a complaint on{" "}
+            <a href="" style={{ textDecoration: "none", opacity: ".8" }}>
+              SEBI SCORES
+            </a>
+            : Register on SCORES portal. Mandatory details for filing complaints
+            on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits:
+            Effective Communication, Speedy redressal of the grievances
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            <a href="" style={{ textDecoration: "none", opacity: ".8" }}>
+              Smart Online Dispute Resolution | Grievances Redressal Mechanism
+            </a>
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            Investments in securities market are subject to market risks; read
+            all the related documents carefully before investing.
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            Attention investors: 1) Stock brokers can accept securities as
+            margins from clients only by way of pledge in the depository system
+            w.e.f September 01, 2020. 2) Update your e-mail and phone number
+            with your stock broker / depository participant and receive OTP
+            directly from depository on your e-mail and/or mobile number to
+            create pledge. 3) Check your securities / MF / bonds in the
+            consolidated account statement issued by NSDL/CDSL every month.
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            India's largest broker based on networth as per NSE.{" "}
+            <a href="" style={{ textDecoration: "none", opacity: ".8" }}>
+              NSE broker factsheet
+            </a>
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            "Prevent unauthorised transactions in your account. Update your
+            mobile numbers/email IDs with your stock brokers. Receive
+            information of your transactions directly from Exchange on your
+            mobile/email at the end of the day. Issued in the interest of
+            investors. KYC is one time exercise while dealing in securities
+            markets - once KYC is done through a SEBI registered intermediary
+            (broker, DP, Mutual Fund etc.), you need not undergo the same
+            process again when you approach another intermediary." Dear
+            Investor, if you are subscribing to an IPO, there is no need to
+            issue a cheque. Please write the Bank account number and sign the
+            IPO application form to authorize your bank to make payment in case
+            of allotment. In case of non allotment the funds will remain in your
+            bank account. As a business we don't give stock tips, and have not
+            authorized anyone to trade on behalf of others. If you find anyone
+            claiming to be part of Zerodha and offering such services, please
+            <a href="" style={{ textDecoration: "none", opacity: ".8" }}>
+              create a ticket here
+            </a>
+            .
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            *Customers availing insurance advisory services offered by Ditto
+            (Tacterial Consulting Private Limited | IRDAI Registered Corporate
+            Agent (Composite) License No CA0738) will not have access to the
+            exchange investor grievance redressal forum, SEBI SCORES/ODR, or
+            arbitration mechanism for such products.
+          </p>
+          <p style={{ fontSize: "12px" }} className="text-muted">
+            Fixed deposit products offered on this platform are third-party
+            products (TPP) and are not Exchange traded products. These are
+            offered through Blostem Fintech Private Limited. Zerodha Broking
+            Limited (SEBI Registration No.: INZ000031633) is acting solely as a
+            distributor for these products. Any disputes arising with respect to
+            such distribution activity will not have access to SEBI SCORES/ODR,
+            Exchange Investor Grievance Redressal Forum, or Arbitration
+            mechanism. Fixed deposits are regulated by the Reserve Bank of India
+            (RBI).
+          </p>
+        </div>
+        <div className="footerLinks">
+          <p className="text-muted" style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              NSE
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              BSE
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              MCX
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              Terms & conditions
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              Policies & procedures
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              Privacy policy
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              Disclosure
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              For investor's attention
+            </a>
+          </p>
+          <p className="text-muted " style={{ fontSize: "12px" }}>
+            <a
+              href=""
+              style={{ textDecoration: "none" }}
+              className="text-muted"
+            >
+              Investor charter
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
