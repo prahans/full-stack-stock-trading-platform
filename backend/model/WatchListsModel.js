669a8a5d7bf431f684b426f92e37910668175e4d
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-05-04 17:24:52 +0545

    storing data of holdings and positions

--- backend/model/WatchListsModel.js
+++
@@ -1,6 +0,0 @@
-const {model} = require("mongoose");
-const {WatchListsSchema} = require("../schemas/WatchListsSchema");
-
-const WatchListsModel = new model ("watchlist", WatchListsSchema);
-
-module.exports = {WatchListsModel}
--- backend/schemas/WatchListsSchema.js
+++
@@ -1,9 +0,0 @@
-const {Schema} = require("mongoose");
-const WatchListsSchema = new Schema({
-    name: String,
-    price: Number,
-    percent: String,
-    isDown: Boolean,
-});
-
-module.exports = {WatchListsSchema};
