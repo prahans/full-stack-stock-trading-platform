e8f671216bde7f20f1cb057408883dc67b693832
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-04 14:30:34 +0545

    making dashboard routes

---
+++ backend/src/routes/dashboardRoutes.ts
@@ -0,0 +1,104 @@
+import { Router } from "express";
+import { type Request, type Response } from "express";
+
+import Post from "../models/posts.ts";
+import Holding from "../models/holdings.ts";
+import Position from "../models/positions.ts";
+import Order from "../models/orders.ts";
+import { userVerification } from "../middlewares/authMiddleware.ts";
+
+const router = Router();
+
+// GET ALL POSTS
+router.get("/", userVerification, async (req: Request, res: Response) => {
+  try {
+    const posts = await Post.find();
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error("Get posts error:", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+router.get(
+  "/allholdings",
+  userVerification,
+  async (req: Request, res: Response) => {
+    try {
+      const allHoldings = await Holding.find({});
+      res.status(200).json(allHoldings);
+    } catch (error) {
+      console.error("Get holdings error:", error);
+
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  },
+);
+
+router.get(
+  "/allpositions",
+  userVerification,
+  async (req: Request, res: Response) => {
+    try {
+      const allPositions = await Position.find({});
+      res.status(200).json(allPositions);
+    } catch (error) {
+      console.error("Get positions error:", error);
+
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  },
+);
+
+router.get(
+  "/allOrders",
+  userVerification,
+  async (req: Request, res: Response) => {
+    try {
+      const allOrders = await Order.find({});
+      res.status(200).json(allOrders);
+    } catch (error) {
+      console.error("Get orders error:", error);
+
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  },
+);
+
+router.post(
+  "/newOrder",
+  userVerification,
+  async (req: Request, res: Response) => {
+    try {
+      let newOrder = new Order({
+        name: req.body.name,
+        qty: req.body.qty,
+        price: req.body.price,
+        mode: req.body.mode,
+      });
+      newOrder.save();
+      res.status(201).send("order saved!");
+    } catch (error) {
+      console.error("Get orders error:", error);
+
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  },
+);
