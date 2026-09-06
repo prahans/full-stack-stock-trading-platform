d95cea5d16cf4e9b4fedde96cbca2a714796e7f2
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-09-06 14:27:34 +0545

    using react-query for fetching data

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,12 +1,23 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./components/Home";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * 60 * 1000,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/*" element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
